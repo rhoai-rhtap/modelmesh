47ba2884c6df1f4fb85b19a7f378e7fbe07b345e
Deepak Chourasia <dchouras@redhat.com>
2024-11-20 16:38:57 +0530

    trying ta

---
+++ restore.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#for i in $(gh pr list | awk '{print $1}');
+#do
+#    gh pr edit $i -t old
+#    gh pr close $i -d
+#done
+#
+#
+#cp -r /home/dchouras/RHODS/DevOps/FBC/operator-back/* .
+git pull --no-edit
+git add .
+git status
+git commit -m "trying ta"
+git push
